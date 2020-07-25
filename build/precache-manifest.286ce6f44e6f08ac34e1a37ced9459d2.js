22904aca5a03b64ff4113358bca1724c5b6d959b
Mustafa <cana66331@gmail.com>
2020-07-25 14:07:15 +0200

    frondend added

---
+++ build/precache-manifest.286ce6f44e6f08ac34e1a37ced9459d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd4a47faa10e8a385a9a644ba1f013cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e899d8728ae77bd7649",
+    "url": "/static/css/main.ba20d5c2.chunk.css"
+  },
+  {
+    "revision": "45d0e15feb43108da5a3",
+    "url": "/static/js/2.b01dda6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b01dda6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e899d8728ae77bd7649",
+    "url": "/static/js/main.cd711112.chunk.js"
+  },
+  {
+    "revision": "e1dd61f92d76ddf53966",
+    "url": "/static/js/runtime-main.cb97306b.js"
+  }
+]);
